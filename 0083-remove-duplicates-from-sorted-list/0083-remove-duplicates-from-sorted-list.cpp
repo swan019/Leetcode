dca4b03e90d8abf119fd82798956298bf4575c4c
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-08-29 02:57:41 +0530

    Time: 8 ms (77.42%), Space: 11.5 MB (63.7%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head == NULL) return NULL;
+        ListNode* curr = head, *stay = head;
+        while(curr != NULL){
+            if(curr->val != stay -> val){
+                stay -> next = curr;
+                stay = curr;
+            }else{
+                curr = curr -> next;
+            }
+        }
+
+        stay -> next = NULL;
+        return head;
+    }
+};

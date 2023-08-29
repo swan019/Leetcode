4a9829afea825e5653767c900f562bf90ea19486
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-08-29 18:48:20 +0530

    Time: 0 ms (100%), Space: 6.1 MB (16.12%) - LeetHub

---
+++ 1446-angle-between-hands-of-a-clock/1446-angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+       
+        double byHrs = hour*30;
+        double byMin = minutes*5.5;
+        double ans;
+        if(byHrs > byMin) 
+            ans = byHrs - byMin;
+        
+        if(ans < 0) ans *= (-1);
+        
+        if(ans > 180)  return 360-ans;
+    
+        cout << endl;
+        return ans;
+    }
+};

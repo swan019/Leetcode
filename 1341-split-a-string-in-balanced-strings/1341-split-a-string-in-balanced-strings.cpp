1a7f5d33a5cbb99edb2523f9ba06eb663427b84e
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-09-13 15:46:35 +0530

    Time: 11 ms (9.67%), Space: 6.6 MB (8.33%) - LeetHub

---
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int balance = 0;
+        int ans = 0;
+        for(int i = 0; i < s.length(); ++i){
+            if(s[i] == 'L') balance++;
+            else balance--;
+
+            if(balance == 0){
+                ans++;
+            }
+            cout << balance << " " << endl;
+
+        }
+
+        return ans;
+    }
+};

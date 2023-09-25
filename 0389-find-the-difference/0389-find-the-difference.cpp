f680cc88d3f86daa6fe8a4d8d3a9d79c63ed7ff5
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-09-25 09:15:32 +0530

    Time: 4 ms (48.11%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,22 +2,34 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         
-        vector<int> hash1(26, 0) , hash2(26,0);
-        for(auto ch : s){
-            hash1[ch-'a']++;
+
+        char xr = '\0';
+        for(char ch : s){
+            xr = xr ^ ch;
         }
-        for(auto ch : t){
-            hash2[ch-'a']++;
+
+        for(char ch : t){
+            xr = xr ^ ch;
         }
-        char ans;
-        for(int i = 0; i < 26; ++i){
-            if(hash1[i] != hash2[i]){
-                ans = 'a' + i;
-                break; 
-            }
-        }
+
+        return xr;
+
+        // vector<int> hash1(26, 0) , hash2(26,0);
+        // for(auto ch : s){
+        //     hash1[ch-'a']++;
+        // }
+        // for(auto ch : t){
+        //     hash2[ch-'a']++;
+        // }
+        // char ans;
+        // for(int i = 0; i < 26; ++i){
+        //     if(hash1[i] != hash2[i]){
+        //         ans = 'a' + i;
+        //         break; 
+        //     }
+        // }
         
-        return ans;
+        // return ans;
 
         // return ' '; 
     }

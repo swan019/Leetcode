d16e58d4705c1b900e767032d4155c076c447c84
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-09-29 09:57:23 +0530

    Time: 7 ms (19.28%), Space: 9.1 MB (7.08%) - LeetHub

---
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pri(arr.size(), 0);
+        pri[0] = arr[0];
+        for(int i = 1; i < arr.size(); ++i){
+            pri[i] = pri[i-1] + arr[i-1];
+        }
+
+        int sum = 0;
+        for(int i = 0; i < n; ++i){
+            for(int j = i; j < n; ++j){
+                    if((j-i + 1) % 2 != 0){
+                        for(int k = i; k <= j; ++k){
+                            sum += arr[k];
+                        }
+                      }
+            }
+        }
+
+        return sum;
+    }
+};

f4f1e2a81382038b8ed54ec799bab95de676c5db
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-09-29 10:02:19 +0530

    Time: 5 ms (35.97%), Space: 8.9 MB (7.08%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.cpp
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.cpp
@@ -10,12 +10,21 @@ public:
 
         int sum = 0;
         for(int i = 0; i < n; ++i){
+            int currsum = 0;
             for(int j = i; j < n; ++j){
+                    // if((j-i + 1) % 2 != 0){
+                    //     for(int k = i; k <= j; ++k){
+                    //         sum += arr[k];
+                    //     }
+                    //   }
+
+                    currsum += arr[j];
+
                     if((j-i + 1) % 2 != 0){
-                        for(int k = i; k <= j; ++k){
-                            sum += arr[k];
-                        }
-                      }
+                        sum += currsum;
+                    }else{
+                        sum += 0;
+                    }
             }
         }
 

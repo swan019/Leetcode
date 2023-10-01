491f55f354af73ced2a5f59572e2c1a3deaea1a2
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-10-01 12:35:29 +0530

    Time: 3 ms (65.92%), Space: 12.7 MB (32.64%) - LeetHub

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> ans;
+        if(root == NULL) return ans;
+        vector<int> ar;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp == NULL){
+                ans.push_back(ar);
+                ar.clear();
+                if(!q.empty()){
+                    q.push(NULL);
+                }
+            }else{
+                ar.push_back(temp->val);
+                if(temp->left != NULL){
+                    q.push(temp->left);
+                }
+
+                if(temp->right != NULL){
+                    q.push(temp->right);
+                }
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

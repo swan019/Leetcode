e3ab2a57d9d49512c052970912e86fbeea67035a
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-10-08 06:48:00 +0530

    Time: 7 ms (68.03%), Space: 20.5 MB (30.03%) - LeetHub

---
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int help(TreeNode* root, int & maxi){
+        if(root == NULL) return 0;
+
+        int lh = help(root->left, maxi);
+        int rh = help(root->right, maxi);
+        maxi = max(maxi, lh+rh);
+        return 1+max(lh, rh);
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxi = 0;
+        int hight = help(root, maxi);
+        return maxi;
+    }
+};

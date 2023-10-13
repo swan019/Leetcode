feecded9d95ad12089636bbc569a99e5a073da02
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-10-13 23:23:52 +0530

    Time: 23 ms (65.75%), Space: 23.7 MB (15.33%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL || root == p || root == q) return root;
+
+        TreeNode* leftsubtree = lowestCommonAncestor(root->left, p, q);
+        TreeNode* rightsubtree = lowestCommonAncestor(root->right, p, q);
+
+        if(leftsubtree == NULL){
+            return rightsubtree;
+        }else if(rightsubtree == NULL){
+            return leftsubtree;
+        }else{
+            return root;
+        }
+    }
+};

d5737be2fe9a1499131663c37b1dd5fb95e86c1b
Swapnil ingale <97128909+swan019@users.noreply.github.com>
2023-10-20 17:38:53 +0530

    Time: 22 ms (45.02%), Space: 21.3 MB (28.96%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -1,40 +1,64 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// private:
+//     void help(TreeNode* root, int targetSum,  int &count, vector<int> path){
+//         if(root == NULL) return;
+
+//         path.push_back(root->val);
+//         help(root->left, targetSum, count, path);
+//         help(root->right, targetSum, count, path);
+
+//         int size = path.size();
+//         long long sum = 0;
+//         for(int i = size-1; i >= 0; i--){
+//             sum += path[i];
+
+//             if(sum == targetSum){
+//                 count++;
+//             }
+//         }
+
+//         path.pop_back();
+//     }
+// public:
+//     int pathSum(TreeNode* root, int targetSum) {
+//         vector<int> path;
+//         int count = 0;
+//         help(root, targetSum, count, path);
+//         return count;
+//     }
+// };
+
 class Solution {
-private:
-    void help(TreeNode* root, int targetSum,  int &count, vector<int> path){
-        if(root == NULL) return;
-
-        path.push_back(root->val);
-        help(root->left, targetSum, count, path);
-        help(root->right, targetSum, count, path);
-
-        int size = path.size();
-        long long sum = 0;
-        for(int i = size-1; i >= 0; i--){
-            sum += path[i];
-
-            if(sum == targetSum){
-                count++;
-            }
-        }
-
-        path.pop_back();
-    }
 public:
+    #define ll long long
+    map<ll ,int> mp;
+    int ans=0;
+    void solve(TreeNode* root, int targetSum,ll currSum){
+        if(root==NULL) return;
+        currSum+=root->val;
+        ans+=mp[currSum-targetSum];//it mean between the ongoing process there is a place where targetSum is generated.
+        mp[currSum]++;
+        solve(root->left , targetSum,currSum);
+        solve(root->right , targetSum,currSum);
+        mp[currSum]--;
+        currSum-=root->val;
+
+    }
     int pathSum(TreeNode* root, int targetSum) {
-        vector<int> path;
-        int count = 0;
-        help(root, targetSum, count, path);
-        return count;
+        mp[0]++;
+        ll currSum=0;
+        solve(root,targetSum,currSum);
+        return ans;
     }
 };
